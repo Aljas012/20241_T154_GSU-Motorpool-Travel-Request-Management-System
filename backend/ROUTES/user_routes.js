b8d793d237c4743eb6ee2743483073bc3089686e
Mart ervin Dahao <2201102996@student.buksu.edu.ph>
2024-10-31 10:25:27 +0800

    update

--- backend/ROUTES/user_routes.js
+++ backend/ROUTES/user_routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getAllUsers,create_user, login_user, createTravelForm, createRequestForm } = require('../CONTROLLERS/user_controller')
 
 
-router.post('/',getAllUsers); //get all users
+router.get('',getAllUsers); //get all users
 router.post('/signup', create_user) //create a data for signup
 router.post('/login',login_user) // route for login
 router.post('/authority_to_travel', createTravelForm); //route for creating authority to travel
